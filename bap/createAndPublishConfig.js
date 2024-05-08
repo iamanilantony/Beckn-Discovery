c3b2a0a6b6cc21fcabdc6ab7121154be592e2850
Anil Antony <anilantony3134@gmail.com>
2024-05-09 00:01:55 +0530

    feat: createAndPublishConfig

---
+++ bap/createAndPublishConfig.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+
+const privateKey = fs.readFileSync('bap_private_key.pem','utf-8')
+const certificate = fs.readFileSync('bap_certificate.pem','utf-8')
+
+const becknJson = {
+    provider: {
+        name: 'xyz',
+        certUrl: 'https://example.com/cert',
+        // ... other provider details
+    },
+    catalog: [
+
+    ],
+    // ..other fields
+}
+
+
+const sign = crypto.createSign('SHA256');
+sign.write(JSON.stringify(becknJson));
+sign.end();
+
+const signature = sign.sign(privateKey, 'base64');
+
+
+const signedBecknJson = {
+    payload: becknJson,
+    signature
+};
+
+
+fs.writeFileSync('beckn.json', JSON.stringify(signedBecknJson));
+
+// (e.g., copy it to a web server's document root to host it)
